8030bd6da8562bfb17f292818b4675d1f5679210
TunchinKao <tunchinkao@gmail.com>
2021-10-22 02:17:30 +0800

    fgawef

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -89,19 +89,19 @@ ExceptionHandler(ExceptionType which)
 		ASSERTNOTREACHED();
 	    break;
 	    case SC_Create:
-		val = kernel->machine->ReadRegister(4);
-		{
-		char *filename = &(kernel->machine->mainMemory[val]);
-		//cout << filename << endl;
-		status = SysCreate(filename);
-		kernel->machine->WriteRegister(2, (int) status);
-		}
-		kernel->machine->WriteRegister(PrevPCReg, kernel->machine->ReadRegister(PCReg));
-		kernel->machine->WriteRegister(PCReg, kernel->machine->ReadRegister(PCReg) + 4);
-		kernel->machine->WriteRegister(NextPCReg, kernel->machine->ReadRegister(PCReg)+4);
-		return;
-		ASSERTNOTREACHED();
-	    break;
+			val = kernel->machine->ReadRegister(4);
+			{
+			char *filename = &(kernel->machine->mainMemory[val]);
+			//cout << filename << endl;
+			status = SysCreate(filename);
+			kernel->machine->WriteRegister(2, (int) status);
+			}
+			kernel->machine->WriteRegister(PrevPCReg, kernel->machine->ReadRegister(PCReg));
+			kernel->machine->WriteRegister(PCReg, kernel->machine->ReadRegister(PCReg) + 4);
+			kernel->machine->WriteRegister(NextPCReg, kernel->machine->ReadRegister(PCReg)+4);
+			return;
+			ASSERTNOTREACHED();
+			break;
       	    case SC_Add:
 		DEBUG(dbgSys, "Add " << kernel->machine->ReadRegister(4) << " + " << kernel->machine->ReadRegister(5) << "\n");
 		/* Process SysAdd Systemcall*/
@@ -143,4 +143,3 @@ ExceptionHandler(ExceptionType which)
     }
     ASSERTNOTREACHED();
 }
-
